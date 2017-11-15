3ecee67e00ee2d0fc21aa431f1af2e2a69fe64c1
Martin Bee <martbee88@gmail.com>
2017-11-15 13:24:30 -0600

    feat: add google creds, add dotenv

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV !== 'production') dotenv.load();
+
+const {
+  GOOGLE_CLIENT,
+  GOOGLE_SECRET,
+} = process.env;
 
 const app = express();
+
+passport.use(new GoogleStrategy());
+
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => res.send({ hi: 'there' }));
-
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
